548245e84b5783150516686ca69baf89e1c26c6c
Sunny <peigangwei2016@gmail.com>
2016-08-27 19:07:14 +0800

    动态添加组件

--- src/main/java/com/sunny/mydemos/MainActivity.java
+++ src/main/java/com/sunny/mydemos/MainActivity.java
@@ -1,37 +1,63 @@
 package com.sunny.mydemos;
 
-import android.content.res.Resources;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+
+    private String TAG = "MainActivity";
+
+    private Button addBut;
+    private LinearLayout ll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        获取Resource中的值
-        Resources resources = getResources();
-//        获取字符串
-        String appName = resources.getString(R.string.app_name);
-
-//        获取颜色
-        int color = resources.getColor(R.color.blue);
-
-//        获取尺寸
-        float dime = resources.getDimension(R.dimen.top_padding);
-
-//        获取bool值
-        boolean isFirstRun = resources.getBoolean(R.bool.is_first_run);
-
-//        获取数组
-        int[] colors = resources.getIntArray(R.array.colors);
-
-//        获取字符串数组
-        String[] names = resources.getStringArray(R.array.names);
+        addBut = (Button) findViewById(R.id.add_but);
+        ll = (LinearLayout) findViewById(R.id.ll);
 
 
+        addBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                新建一个Button
+                Button button = new Button(MainActivity.this);
+                button.setText("我是新添加的Button");
+//              将新建的Button添加到LinerLayout中
+                ll.addView(button, WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+
+//                button.clearFocus();
+////                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+////                button.setLayoutParams(params);
+//                WindowManager windowManager = getWindowManager();
+//
+//                WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+//                params.width= WindowManager.LayoutParams.MATCH_PARENT;
+//                params.height= WindowManager.LayoutParams.WRAP_CONTENT;
+//                params.x=0;
+//                params.y=0;
+//                windowManager.addView(button,params);
+            }
+        });
 
     }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: ");
+    }
+
+
 }
---
+++ src/main/java/com/sunny/mydemos/MyApplication.java
@@ -0,0 +1,49 @@
+package com.sunny.mydemos;
+
+import android.app.Application;
+import android.content.res.Configuration;
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2016/8/27.
+ */
+public class MyApplication extends Application {
+
+    private String TAG ="MyApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        "On Create".to
+
+        Log.d(TAG, "MyApplication onCreate: ");
+
+
+    }
+
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+
+        Log.d(TAG, "onTerminate: ");
+
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+
+        Log.d(TAG, "onLowMemory: ");
+
+
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.d(TAG, "onConfigurationChanged: ");
+
+    }
+}

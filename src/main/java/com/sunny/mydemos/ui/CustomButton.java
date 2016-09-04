bd03b8eb0b503dea45ac2d3440d524821e3dd071
Sunny <peigangwei2016@gmail.com>
2016-09-04 19:56:03 +0800

    View的事件练习

--- src/main/java/com/sunny/mydemos/ui/CustomButton.java
+++ src/main/java/com/sunny/mydemos/ui/CustomButton.java
@@ -1,55 +1,95 @@
 package com.sunny.mydemos.ui;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Build;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2016/9/4.
  */
 public class CustomButton extends View {
-    private int mIndex=0;
-    private String TAG ="CustomButton";
+    private int mIndex = 0;
+    private String TAG = "CustomButton";
 
     public CustomButton(Context context) {
         super(context);
+//        设置焦点
+        setFocusable(true);
+        setFocusableInTouchMode(true);
     }
 
+
+    //    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if(mIndex>100)mIndex=0;
+        if (mIndex > 100) mIndex = 0;
 
         ++mIndex;
 
 //        定义画笔
-        Paint paint=new Paint();
+        Paint paint = new Paint();
 
-        Log.i(TAG, "onDraw: "+mIndex);
+        Log.i(TAG, "onDraw: " + mIndex);
 
 //        给画笔设置颜色
-        switch (mIndex%4){
-            case 1:
-                paint.setColor(Color.RED);
-                break;
-            case 2:
-                paint.setColor(Color.GREEN);
-                break;
-            case 3:
-                paint.setColor(Color.BLUE);
-                break;
-            default:
-                paint.setColor(Color.GRAY);
-                break;
-        }
+        paint.setColor(Color.GRAY);
+//        switch (mIndex%4){
+//            case 1:
+//                paint.setColor(Color.RED);
+//                break;
+//            case 2:
+//                paint.setColor(Color.GREEN);
+//                break;
+//            case 3:
+//                paint.setColor(Color.BLUE);
+//                break;
+//            default:
+//                paint.setColor(Color.GRAY);
+//                break;
+//        }
 
 
 //        画一个正方形
-        canvas.drawRect(0,0,120,60,paint);
+//        canvas.drawRoundRect(20,20,160,120,5,5,paint);
+        canvas.drawRect(20, 20, 160, 100, paint);
+        paint.setColor(Color.BLACK);
+        paint.setTextSize(30);
+        canvas.drawText("Login", 50, 70, paint);
 
+    }
 
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Log.i(TAG, "CustomButton  onKeyUp: ");
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.i(TAG, "onTouchEvent:  onClick");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.i(TAG, "onTouchEvent: x=" + x + "  y=" + y);
+                break;
+
+        }
+
+        return super.onTouchEvent(event);
     }
 }
--- src/main/java/com/sunny/mydemos/ui/UiActivity.java
+++ src/main/java/com/sunny/mydemos/ui/UiActivity.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 /**
  * Created by Administrator on 2016/9/4.
@@ -33,14 +39,51 @@ public class UiActivity extends Activity {
     };*/
 
     @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        Log.i(TAG, "UiActivity  onKeyUp: ");
+        return super.onKeyUp(keyCode, event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.i(TAG, "onTouchEvent:  onClick");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.i(TAG, "onTouchEvent: x=" + x + "  y=" + y);
+                break;
+            case MotionEvent.ACTION_UP:
+                if((event.getEventTime()-event.getDownTime())>2000){
+                    Log.i(TAG, "you Long Click! ");
+                }
+                break;
+
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mButton = new CustomButton(this);
+         mButton = new CustomButton(this);
+        /*LinearLayout linearLayout = new LinearLayout(this);
+        ViewGroup.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setLayoutParams(params);
+
+        Button button=new Button(this);
+        button.setText("Login");
+        linearLayout.addView(button, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);*/
+
         setContentView(mButton);
 
 //        mHandler.sendMessageDelayed(mHandler.obtainMessage(),200);
 //            mHandler.postDelayed(mRunnable,200);
-        new Thread(new Runnable() {
+       /* new Thread(new Runnable() {
             @Override
             public void run() {
                 while(!Thread.currentThread().isInterrupted()){
@@ -52,6 +95,6 @@ public class UiActivity extends Activity {
                     mButton.postInvalidate();
                 }
             }
-        }).start();
+        }).start();*/
     }
 }

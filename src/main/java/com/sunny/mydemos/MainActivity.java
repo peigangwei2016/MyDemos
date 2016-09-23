f2f9c5e3bf9205782ca1475d9211550c376b5425
Sunny <peigangwei2016@gmail.com>
2016-09-24 00:44:45 +0800

    Enpty Project

--- src/main/java/com/sunny/mydemos/MainActivity.java
+++ src/main/java/com/sunny/mydemos/MainActivity.java
@@ -11,27 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        获取Resource中的值
-        Resources resources = getResources();
-//        获取字符串
-        String appName = resources.getString(R.string.app_name);
-
-//        获取颜色
-        int color = resources.getColor(R.color.blue);
-
-//        获取尺寸
-        float dime = resources.getDimension(R.dimen.top_padding);
-
-//        获取bool值
-        boolean isFirstRun = resources.getBoolean(R.bool.is_first_run);
-
-//        获取数组
-        int[] colors = resources.getIntArray(R.array.colors);
-
-//        获取字符串数组
-        String[] names = resources.getStringArray(R.array.names);
-
-
 
     }
 }
